086bf4126989be16096198e719013809502728a5
iodunsi <io6@njit.edu>
2024-06-11 16:45:26 -0400

    testing file

---
+++ myproject/calculator/calctest.py
@@ -0,0 +1,13 @@
+from .calc import Calculator
+
+print(Calculator.add(2,3)) #Making sure adding works
+print(Calculator.subtract(5,3)) #Making sure subtracting works
+print(Calculator.multiply(2,3)) #Making sure multiplting works
+print(Calculator.divide(6,3)) #Making sure division works
+print(Calculator.divide(2,0)) #Testing division by zero
+
+
+
+
+
+
