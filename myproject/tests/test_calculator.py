2675867712497d117e36c8f86aa94a947e86c88d
iodunsi <io6@njit.edu>
2024-06-12 21:44:53 -0400

    test file debug

--- myproject/tests/test_calculator.py
+++ myproject/tests/test_calculator.py
@@ -1,23 +1,48 @@
 '''My Calculator Test'''
 from calculator import add, subtract, multiply, divide
-
+#assert("Testing this file out")
 def test_addition():
     '''Test that addition function works '''    
-    print (add(2,2)) #== 4)
+    assert add (2,2)
 
 def test_subtraction():
     '''Test that - function works '''    
-    print (subtract(2,2)) #== 0
+    assert (subtract(2,2==0))
 
 def test_multi():
     '''Test that x function works '''    
-    print (multiply(2,2)) #== 4
+    assert multiply(2, 2) == 4
 
 def test_div():
     try:
         result=divide(2,0)
     except ValueError as e:
-          print ("Can't divide by zero")
+          assert ("Can't divide by zero")
     '''Test that / function works '''    
-    print (divide(2,2)) 
+    assert (divide(2,2)) 
+
+if __name__ == "__main__":
+    try:
+        test_addition()
+        print("Addition works")
+    except AssertionError:
+        print("Something went wrong.")
+
+    try:
+        test_subtraction()
+        print("Subtraction works")
+    except AssertionError:
+        print("Something went wrong.")
+
+    try:
+        test_multi()
+        print("Multiplication works")
+    except AssertionError:
+        print("Something went wrong.")
+
+    try:
+        test_div()
+        print("Division works")
+    except AssertionError:
+        print("Something went wrong.")
 
