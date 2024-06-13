3cbe8134bc1161dbb6c82a7db4ee5d1fadaac7d9
iodunsi <io6@njit.edu>
2024-06-13 19:10:47 -0400

    different approach to test calc file

--- myproject/tests/test_calculator.py
+++ myproject/tests/test_calculator.py
@@ -5,4 +5,8 @@ from calculator import add, subtract, multiply, divide
 @pytest.mark.parametrize("num1, num2, operation, outcome", [])
 def test_operations(num1, num2, operation, outcome):
     '''Test arithmetic operatios'''
-    assert operation(num1, num2) == outcome 
+    if operation == "divide" and num2 == 0:
+        with pytest.raises(ZeroDivisionError):
+            divide (num1, num2)
+    else:
+        assert operation(num1,num2) == outcome
