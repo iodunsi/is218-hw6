0b3312602c1b6e599824987a21bfbc10576bcbff
iodunsi <io6@njit.edu>
2024-06-13 22:32:11 -0400

    final edits, all tests passed hw 4, test_calc file

--- myproject/tests/test_calculator.py
+++ myproject/tests/test_calculator.py
@@ -1,12 +1,12 @@
 '''My Calculator Test'''
 import pytest
-from calculator import add, subtract, multiply, divide
+from calculator import divide
 #from calculator import Calculator
-@pytest.mark.parametrize("num1, num2, operation, outcome", [])
+#@pytest.mark.parametrize("num1, num2, op_name, operation, outcome", [])
 def test_operations(num1, num2, op_name, operation, outcome):
     '''Test arithmetic operatios'''
     if op_name == "divide" and num2 == 0:
-        with pytest.raises(ZeroDivisionError):
+        with pytest.raises(ValueError):
             divide (num1, num2)
     else:
         assert operation(num1,num2) == outcome
