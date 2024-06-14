d35d01fa489f9a120b3aa31adbb97d915fecbf45
iodunsi <io6@njit.edu>
2024-06-13 21:21:21 -0400

    need to mod again

--- myproject/tests/test_calculator.py
+++ myproject/tests/test_calculator.py
@@ -3,9 +3,9 @@ import pytest
 from calculator import add, subtract, multiply, divide
 #from calculator import Calculator
 @pytest.mark.parametrize("num1, num2, operation, outcome", [])
-def test_operations(num1, num2, operation, outcome):
+def test_operations(num1, num2, op_name, operation, outcome):
     '''Test arithmetic operatios'''
-    if operation == "divide" and num2 == 0:
+    if op_name == "divide" and num2 == 0:
         with pytest.raises(ZeroDivisionError):
             divide (num1, num2)
     else:
